fc3778cbbb583cd07b1345053233f096a1a6798d
Fabien Alet <alet@irsamc.ups-tlse.fr>
2024-10-21 17:44:38 +0200

    d

--- ed_XXZ_si_prod.cc
+++ ed_XXZ_si_prod.cc
@@ -467,9 +467,11 @@ int ENV_NUM_THREADS=omp_get_num_threads();
          //   VecGetValues( xr, 1, row_ctr, &pi );
          //   cout << "A " << pi << endl;
             VecGetValues( xr, 1, &row_ctr, &pi );
-         //   cout << "B " << pi << endl;
+            cout << "B " << pi << endl;
              if ((pi != 0)) {
+             
              local_S1 -= 2.0* pi * pi * log(pi);
+              cout << pi << " " << local_S1 << endl;
               }
           }
           double global_S1=0.;
