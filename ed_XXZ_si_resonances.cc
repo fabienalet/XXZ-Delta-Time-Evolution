a143524860fa876e2aa272a8dbf3f787a80502b2
Fabien Alet <alet@irsamc.ups-tlse.fr>
2024-11-04 19:06:39 +0100

    d

--- ed_XXZ_si_resonances.cc
+++ ed_XXZ_si_resonances.cc
@@ -305,7 +305,9 @@ int ENV_NUM_THREADS=omp_get_num_threads();
 
       std::vector<int> eigenstates_to_follow;
       std::vector<double> energies_to_follow;
-      
+      std::vector< std::vector<int> > sites_to_follow;
+      std::vector< std::vector<pair<int,int> > > pairs_to_follow;
+      std::vector< std::vector<double> > sz_to_follow;
 
       PetscBool debug=PETSC_FALSE;
       PetscOptionsGetBool(NULL, NULL, "-debug", &debug,NULL);
@@ -351,10 +353,6 @@ int ENV_NUM_THREADS=omp_get_num_threads();
         sz_product_cutoff*=4;
 
       for (int i = 0; i < nconv; i++) {
-        std::vector< std::vector<int> > sites_to_follow;
-        std::vector< std::vector<pair<int,int> > > pairs_to_follow;
-        std::vector< std::vector<double> > sz_to_follow;
-
         ierr = EPSGetEigenpair(eps2, i, &Er, &Ei, xr, NULL);
         CHKERRQ(ierr);
         energies.push_back(PetscRealPart(Er));
@@ -396,6 +394,9 @@ int ENV_NUM_THREADS=omp_get_num_threads();
         if (prediction_site.size()!=0) { eigenstates_to_follow.push_back(i); sites_to_follow.push_back(prediction_site); 
         energies_to_follow.push_back(Er); sz_to_follow.push_back(sz);
         if (prediction_strong_correl_pair.size()!=0) { pairs_to_follow.push_back(prediction_strong_correl_pair);}
+        cout << i << " " << sites_to_follow.size() << " " << sz_to_follow.size() << " " << prediction_strong_correl_pair.size() << endl;
+
+
         }
       
 
