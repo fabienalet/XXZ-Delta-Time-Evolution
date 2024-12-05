29eb0d5c6931c95c6b6812a161792b9bc77c3d36
Fabien Alet <alet@irsamc.ups-tlse.fr>
2024-12-05 17:17:03 +0100

    d

--- ed_XXZ_si_resonances.cc
+++ ed_XXZ_si_resonances.cc
@@ -643,10 +643,10 @@ int ENV_NUM_THREADS=omp_get_num_threads();
            VecPointwiseMult(use2,use1,xr);
            int s=sites_to_follow[ll].size();
             std::vector<double> sigma_indicator(s,0.);
-          for (int si=0;si<s;++si) {
-            int k=(int) sites_to_follow[ll][si];
+          //for (int si=0;si<s;++si) {
+          //  int k=(int) sites_to_follow[ll][si];
           //  VecDot(use2,sigmas_as_vec[k],&sigma_indicator[si]);
-          }
+          //}
         // HERE !
         /*
         for (int si=0;si<s;++si) {
