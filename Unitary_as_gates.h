5d0358160fb24ebcf5901dbee9c766b86124f78f
Fabien Alet <alet@irsamc.ups-tlse.fr>
2025-03-07 16:02:20 +0100

    d

--- Unitary_as_gates.h
+++ Unitary_as_gates.h
@@ -477,9 +477,10 @@ PetscErrorCode Unitary_as_gates::init()
       if (b[r]==b[(r+1+Lchain_)%Lchain_]) { ising_energy+=J_coupling[r];} else { ising_energy-=J_coupling[r];} 
       }
     PetscReal angle_ising=ising_energy;
+
     PetscScalar matrix_element=cos(angle_ising)+PETSC_i*sin(angle_ising);
     VecSetValues(_CTX->Ising_gate, 1, &i, &matrix_element, INSERT_VALUES);
-    cout << "me=" << matrix_element << endl;
+    cout << "ange= " << angle_ising << " me=" << matrix_element << endl;
   }
   VecAssemblyBegin(_CTX->Ising_gate); VecAssemblyEnd(_CTX->Ising_gate);
 
