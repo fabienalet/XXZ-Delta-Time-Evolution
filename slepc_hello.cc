960b34b09fe3d183f60966bae48073b022228d8e
Fabien Alet <alet@irsamc.ups-tlse.fr>
2025-03-13 14:31:29 +0100

    d

--- slepc_hello.cc
+++ slepc_hello.cc
@@ -1,104 +1,11 @@
-static char help[] =
-    "Shift-Invert ED for XXZ chain with Fibonnaci fields \n (C) Fabien Alet "
-      "\n";
-
-// #define PETSC_DESIRE_COMPLEX
-// #define PETSC_USE_COMPLEX
-// #include <complex>
-#include <omp.h>
-//#include <slepceps.h>
-#include <petscksp.h>
-
-#include <fstream>
-#include <iostream>
-using namespace std;
-/*
-#include "Spin/Spin_basis.h"
-
-#include "Spin/Spin_parameters.h"
-
-#include "Spin/SpinOneHalfXXZ_disorder.h"
-
-#include "Spin/Spin_observable.h"
-*/
-#undef __FUNCT__
-#define __FUNCT__ "main"
-
-int main(int argc, char **argv) {
-  cout.precision(20);
-  PetscInitialize(&argc, &argv, "slepc.options", help);
-  int myrank, mpisize;
-  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-  MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
-  cout << "myrank=" << myrank << " says hello\n";
-  cout << "I say hello\n";
-/*
-  int ENV_NUM_THREADS=omp_get_num_threads();
-  // For parallelization between nodes (MPI)
-  
-
-  Mat H;
-  EPS eps;
-  EPSType type;
-  PetscErrorCode ierr;
-  PetscReal error, tol, re, im;
-
-  int number_of_states = 2;
-  double Sz = 0;
-  ierr = PetscOptionsGetReal(NULL, NULL, "-Sz", &Sz, NULL);
-  CHKERRQ(ierr);
-  
-  Parameters myparameters(myrank);
-  int L = myparameters.L;
-  basis mybasis(L, Sz, myrank, number_of_states);
-  //  mybasis.check_basis(); // ??
-  int nconf = mybasis.total_number_of_confs;
-
-  if (myrank == 0) {
-    std::cout << "# L = " << L << " number of states=" << nconf << std::endl;
-  }
-  observable myobservable(&mybasis, ENV_NUM_THREADS);
-
-  PetscInt Istart, Iend;
-  {
-    std::vector<int> local_block_sizes(mpisize, nconf / mpisize);
-    for (size_t i = 0; i < nconf % mpisize; i++)
-      local_block_sizes[i]++;  // distribute evenly
-    Istart = 0;
-    for (size_t i = 0; i < myrank; i++) Istart += local_block_sizes[i];
-    Iend = Istart + local_block_sizes[myrank];
-
-    Hamiltonian myHamiltonian(&mybasis, &H);
-    myHamiltonian.get_parameters();
-
-    myparameters.string_from_basis = mybasis.string_names;
-    myparameters.string_from_H = myHamiltonian.string_names;
-
-    //	 myobservable.get_local_field_x(myHamiltonian.local_field_x);
-    // myobservable.get_local_field_z(myHamiltonian.local_field_z);
-    myHamiltonian.init_matrix_sizes(Istart, Iend);
-    ierr = MatSetUp(H);
-    CHKERRQ(ierr);
-    myHamiltonian.create_matrix(Istart, Iend);
-
-    if (myrank == 0){ 
-      myHamiltonian.print_local_fields();
-    }
-
-  }
-
-  MatSetOption(H, MAT_SYMMETRIC, PETSC_TRUE);
-  MatSetOption(H, MAT_SYMMETRY_ETERNAL, PETSC_TRUE);
-  if (myrank == 0) std::cout << "# Assembly... " << std::flush;
-  ierr = MatAssemblyBegin(H, MAT_FINAL_ASSEMBLY);
-  CHKERRQ(ierr);
-  ierr = MatAssemblyEnd(H, MAT_FINAL_ASSEMBLY);
-  CHKERRQ(ierr);
-  if (myrank == 0) {
-    std::cout << "Hamiltonian matrix assembly done." << std::endl;
-  }
-*/
-  // SlepcFinalize();
-  PetscFinalize();
-  return 0;
+/* minimal petsc program */
+/* adapted (stolen) from http://petsc.wordpress.com/2012/07/23/1-hello-world/ */
+#include <petsc.h>
+int main( int argc, char *argv[]) {
+	PetscInitialize(&argc,&argv,PETSC_NULL,PETSC_NULL);
+	PetscPrintf(PETSC_COMM_WORLD,"Hello World\n");
+	PetscFinalize();
+	return 0;
 }
+
+
