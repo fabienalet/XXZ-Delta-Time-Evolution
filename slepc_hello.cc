95208669408dc413fd7a6932f7399209f501c73a
Fabien Alet <alet@irsamc.ups-tlse.fr>
2025-03-13 14:09:45 +0100

    slepc_hello

---
+++ slepc_hello.cc
@@ -0,0 +1,129 @@
+static char help[] =
+    "Shift-Invert ED for XXZ chain with Fibonnaci fields \n (C) Fabien Alet "
+    "2017. \n\n"
+    "The command line options are:\n"
+    "  -L <L>, where <L> = size of the chain [default 6]\n"
+    "  ( .... Description not complete ....) \n"
+    "\n";
+
+// #define PETSC_DESIRE_COMPLEX
+// #define PETSC_USE_COMPLEX
+// #include <complex>
+#include <omp.h>
+#include <slepceps.h>
+#include <boost/dynamic_bitset.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/uniform_int_distribution.hpp>
+#include <boost/random/uniform_real_distribution.hpp>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <random>
+#ifdef USE_MKL
+#include <mkl.h>
+#include <mkl_cblas.h>
+#include <mkl_lapack.h>
+#else
+typedef struct __MKL_Complex16 {
+  double real;
+  double imag;
+} MKL_Complex16;
+typedef int MKL_INT;
+typedef MKL_Complex16 lapack_complex_double;
+
+#include <cblas.h>
+#include <lapacke.h>
+#endif
+
+double PI = acos(-1.);
+using namespace std;
+
+#include "Spin/Spin_basis.h"
+
+#include "Spin/Spin_parameters.h"
+
+#include "Spin/SpinOneHalfXXZ_disorder.h"
+
+#include "Spin/Spin_observable.h"
+
+#undef __FUNCT__
+#define __FUNCT__ "main"
+
+int main(int argc, char **argv) {
+  cout.precision(20);
+  SlepcInitialize(&argc, &argv, "slepc.options", help);
+
+  int ENV_NUM_THREADS=omp_get_num_threads();
+  // For parallelization between nodes (MPI)
+  int myrank, mpisize;
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
+
+  /***** Petsc / Slepc data structure ***/
+  Mat H;
+  EPS eps;
+  EPSType type;
+  PetscErrorCode ierr;
+  PetscReal error, tol, re, im;
+
+  int number_of_states = 2;
+  double Sz = 0;
+  ierr = PetscOptionsGetReal(NULL, NULL, "-Sz", &Sz, NULL);
+  CHKERRQ(ierr);
+  cout << "myrank=" << myrank << " says hello\n";
+  /*
+  Parameters myparameters(myrank);
+  int L = myparameters.L;
+  basis mybasis(L, Sz, myrank, number_of_states);
+  //  mybasis.check_basis(); // ??
+  int nconf = mybasis.total_number_of_confs;
+
+  if (myrank == 0) {
+    std::cout << "# L = " << L << " number of states=" << nconf << std::endl;
+  }
+  observable myobservable(&mybasis, ENV_NUM_THREADS);
+
+  PetscInt Istart, Iend;
+  {
+    std::vector<int> local_block_sizes(mpisize, nconf / mpisize);
+    for (size_t i = 0; i < nconf % mpisize; i++)
+      local_block_sizes[i]++;  // distribute evenly
+    Istart = 0;
+    for (size_t i = 0; i < myrank; i++) Istart += local_block_sizes[i];
+    Iend = Istart + local_block_sizes[myrank];
+
+    Hamiltonian myHamiltonian(&mybasis, &H);
+    myHamiltonian.get_parameters();
+
+    myparameters.string_from_basis = mybasis.string_names;
+    myparameters.string_from_H = myHamiltonian.string_names;
+
+    //	 myobservable.get_local_field_x(myHamiltonian.local_field_x);
+    // myobservable.get_local_field_z(myHamiltonian.local_field_z);
+    myHamiltonian.init_matrix_sizes(Istart, Iend);
+    ierr = MatSetUp(H);
+    CHKERRQ(ierr);
+    myHamiltonian.create_matrix(Istart, Iend);
+
+    if (myrank == 0){ 
+      myHamiltonian.print_local_fields();
+    }
+
+  }
+
+  MatSetOption(H, MAT_SYMMETRIC, PETSC_TRUE);
+  MatSetOption(H, MAT_SYMMETRY_ETERNAL, PETSC_TRUE);
+  if (myrank == 0) std::cout << "# Assembly... " << std::flush;
+  ierr = MatAssemblyBegin(H, MAT_FINAL_ASSEMBLY);
+  CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(H, MAT_FINAL_ASSEMBLY);
+  CHKERRQ(ierr);
+  if (myrank == 0) {
+    std::cout << "Hamiltonian matrix assembly done." << std::endl;
+  }
+*/
+  SlepcFinalize();
+  return 0;
+}
